6a197527c07decb60d4b92db8fe1a41a69f35aa9
Mark Strickland <mstrickland@globys.com>
2019-03-22 12:16:49 -0700

    Add method and test for the age of the user on Earth

--- spec/age-calculator-spec.js
+++ spec/age-calculator-spec.js
@@ -5,7 +5,15 @@ describe('AgeCalculator', function() {
   it('should test whether an AgeCalculator constructor works', function() {
     let testDate = "1962-05-03";
     let userAge = new AgeCalculator(testDate);
-    expect(userAge.userBirthDate).toEqual(new Date(1962, 04, 03));
+
+    expect(userAge.userBirthDate).toEqual(new Date(1962, 4, 3));
+  });
+
+  it('should calculate the age of the user on Earth', function() {
+    let testDate = "1962-05-03";
+    let userAge = new AgeCalculator(testDate);
+
+    expect(userAge.userAgeEarth()).toEqual(56);
   });
 
 })
--- src/agecalculator.js
+++ src/agecalculator.js
@@ -15,34 +15,4 @@ export default class AgeCalculator {
     return Math.floor((new Date() - this.userBirthDate) / 31536000000);
   }
 
-  // constructor(myDate) {
-  //   this.myDate = myDate;
-  //   this.year = parseInt(this.myDate.substring(0,4));
-  //   this.month = parseInt(this.myDate.substring(5,7));
-  //   this.day = parseInt(this.myDate.substring(8,10));
-  // }
-  //
-  // isLeapYear() {
-  //   if (this.year % 4 === 0 && (this.year % 100 != 0 || this.year % 400 === 0)) {
-  //       return true;
-  //   }
-  //   else {
-  //       return false;
-  //   }
-  // }
-  //
-  // get dayOfWeek() {
-  //   const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  //   let monthDays = [];
-  //
-  //   if (this.isLeapYear()) {
-  //     monthDays = [0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335];
-  //   } else {
-  //     monthDays = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
-  //   }
-  //
-  //   let days = ((this.year - 1) * 365) + Math.floor(((this.year - 1) / 4)) - Math.floor(((this.year - 1) / 100)) + Math.floor(((this.year - 1) / 400))  + monthDays[this.month - 1] + this.day;
-  //
-  //   return daysOfWeek[(days % 7)];
-  // }
 }
